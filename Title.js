a2c97c4e682b58751363222d2410987020085a14
akhi799 <74193911+akhi799@users.noreply.github.com>
2021-10-24 20:28:05 +0530

    Add files via upload

---
+++ Title.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './Title.css';
+
+export default function Title() {
+    return (
+        <div className="Title">
+            <h1>Title</h1>
+            <div className="Title-Subtitle">San Franciscos Privately Owned Public Spaces</div>
+        </div>
+    )
+}
